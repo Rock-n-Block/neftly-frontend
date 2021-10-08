8fe448a35a9bb2d2848c6b9c4c34f55226da80a8
TheJuze <dimanikus0106@gmail.com>
2021-10-08 17:59:34 +0300

    вынес конфиг для карусели в отдельный файл

---
+++ src/forms/Create/component/ChooseCollection/carouselBreakpointsConfig.ts
@@ -0,0 +1,51 @@
+export default [
+  {
+    breakpoint: 1281,
+    settings: {
+      slidesToShow: 5,
+      slidesToScroll: 5,
+    },
+  },
+  {
+    breakpoint: 1000,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    },
+  },
+  {
+    breakpoint: 840,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    },
+  },
+  {
+    breakpoint: 680,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 510,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 481,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    },
+  },
+  {
+    breakpoint: 375,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+];
--- src/forms/Create/component/ChooseCollection/index.tsx
+++ src/forms/Create/component/ChooseCollection/index.tsx
@@ -6,6 +6,7 @@ import { Carousel, Modal, Text } from 'components';
 import { observer } from 'mobx-react';
 import { userApi } from 'services/api';
 import { useMst } from 'store';
+import carouselBreakpointsConfig from './carouselBreakpointsConfig';
 
 import { CreateCollection } from '../../../index';
 
@@ -72,61 +73,7 @@ const ChooseCollection: React.FC<IProps> = observer(
     return (
       <div className={cn(styles.cards, className)}>
         {/* TODO: убрать выезды за layout */}
-        <Carousel
-          hideArrows
-          slidesToShow={3}
-          responsive={[
-            {
-              breakpoint: 1281,
-              settings: {
-                slidesToShow: 5,
-                slidesToScroll: 5,
-              },
-            },
-            {
-              breakpoint: 1000,
-              settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-              },
-            },
-            {
-              breakpoint: 840,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-              },
-            },
-            {
-              breakpoint: 680,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-              },
-            },
-            {
-              breakpoint: 510,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 481,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-              },
-            },
-            {
-              breakpoint: 375,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              },
-            },
-          ]}
-        >
+        <Carousel hideArrows slidesToShow={3} responsive={carouselBreakpointsConfig}>
           <div
             onClick={handleOpenModal}
             onKeyDown={handleOpenModal}
