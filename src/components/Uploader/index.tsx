3e92aab06c07793b52432f3b87048e9a516ecf6e
TheJuze <dimanikus0106@gmail.com>
2021-10-08 12:41:18 +0300

    Uploader type updates

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { message, Upload } from 'antd';
 import cn from 'classnames';
 import { useFormikContext } from 'formik';
@@ -29,59 +29,76 @@ const Uploader: React.FC<IUploader> = ({
   isLoading,
 }) => {
   const formik = useFormikContext();
+  const MAX_FILE_SIZE = 30;
   // const [imageUrl, setImageUrl] = React.useState('');
-  const getBase64 = (img: any, callback: any) => {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      if (type === 'img') formik.setFieldValue('preview', reader.result);
-      if (type === 'cover') formik.setFieldValue('coverPreview', reader.result);
-      callback(reader.result);
-    });
-    reader.readAsDataURL(img);
-  };
-  const beforeUpload = (file: any) => {
-    const isValidType =
-      file.type === 'image/jpeg' ||
-      file.type === 'image/png' ||
-      file.type === 'image/webp' ||
-      file.type === 'image/gif' ||
-      (type === 'img' && file.type === 'video/mp4') ||
-      (type === 'img' && file.type === 'audio/mpeg');
-    if (!isValidType) {
-      message.error('You can only upload JPG/PNG/WEBP/GIF file!');
-    }
-    const isLt2M = file.size / 1024 / 1024 <= 30;
-    if (!isLt2M) {
-      message.error('Image must be smaller than 30MB!');
-    }
-    return isValidType && isLt2M;
-  };
-  const handleChange = ({ file }: any) => {
-    if (isLoading) {
-      return;
-    }
-    const isValidType =
-      file.type === 'image/jpeg' ||
-      file.type === 'image/png' ||
-      file.type === 'image/webp' ||
-      file.type === 'image/gif' ||
-      (type === 'img' && file.type === 'video/mp4') ||
-      (type === 'img' && file.type === 'audio/mpeg');
-    if (!isValidType) {
-      return;
-    }
-    const isLt2M = file.size / 1024 / 1024 < 30;
-    if (!isLt2M) {
-      return;
-    }
-    if (type === 'img' && setFormat) setFormat(file.type.slice(0, file.type.indexOf('/')));
-    if (handleUpload) {
-      handleUpload(file.originFileObj);
-    } else {
-      formik.setFieldValue(type, file.originFileObj);
-      getBase64(file.originFileObj, () => {});
-    }
-  };
+  const getBase64 = useCallback(
+    (img: any, callback: any) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        if (type === 'img') formik.setFieldValue('preview', reader.result);
+        if (type === 'cover') formik.setFieldValue('coverPreview', reader.result);
+        callback(reader.result);
+      });
+      reader.readAsDataURL(img);
+    },
+    [formik, type],
+  );
+  const checkIsValidType = useCallback(
+    (file: any) => {
+      if (!file) {
+        return false;
+      }
+      const isValidType =
+        file.type === 'image/jpeg' ||
+        file.type === 'image/jpg' ||
+        file.type === 'image/svg' ||
+        file.type === 'image/svg+xml' ||
+        file.type === 'image/png' ||
+        file.type === 'image/webp' ||
+        file.type === 'image/gif' ||
+        (type === 'img' && file.type === 'video/mp4') ||
+        (type === 'img' && file.type === 'audio/mpeg');
+      return isValidType;
+    },
+    [type],
+  );
+  const beforeUpload = useCallback(
+    (file: any) => {
+      const isValidType = checkIsValidType(file);
+      if (!isValidType) {
+        message.error('You can only upload JPG/PNG/WEBP/GIF file!');
+      }
+      const isLt2M = file.size / 1024 / 1024 <= MAX_FILE_SIZE;
+      if (!isLt2M) {
+        message.error('Image must be smaller than 30MB!');
+      }
+      return isValidType && isLt2M;
+    },
+    [checkIsValidType],
+  );
+  const handleChange = useCallback(
+    ({ file }: any) => {
+      if (isLoading) {
+        return;
+      }
+      const isValidType = checkIsValidType(file);
+      if (!isValidType) {
+        return;
+      }
+      const isLt2M = file.size / 1024 / 1024 < MAX_FILE_SIZE;
+      if (!isLt2M) {
+        return;
+      }
+      if (type === 'img' && setFormat) setFormat(file.type.slice(0, file.type.indexOf('/')));
+      if (handleUpload) {
+        handleUpload(file.originFileObj);
+      } else {
+        formik.setFieldValue(type, file.originFileObj);
+        getBase64(file.originFileObj, () => {});
+      }
+    },
+    [checkIsValidType, formik, getBase64, handleUpload, isLoading, setFormat, type],
+  );
   return (
     <div className={cn(className, !isButton ? styles.uploader : '')}>
       {isButton ? (
