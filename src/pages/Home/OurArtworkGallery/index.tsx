acb30d6afefdc55886b9cf86d58eb698d1866e5f
TheJuze <dimanikus0106@gmail.com>
2021-10-13 13:45:17 +0300

    created load more hook, added Our Artwork Gallery API integration

--- src/pages/Home/OurArtworkGallery/index.tsx
+++ src/pages/Home/OurArtworkGallery/index.tsx
@@ -2,17 +2,27 @@ import { FC, useState } from 'react';
 import cx from 'classnames';
 import { ArtCard, Button, H2, Loader } from 'components';
 
-// import { data as mockData } from './mockData';
-
 import styles from './styles.module.scss';
+import { useFetchNft, useLoadMore } from 'hooks';
 
 type Props = {
   className?: string;
 };
 
 const OurArtworkGallery: FC<Props> = ({ className }) => {
-  const [currentPage, setCurrentPage] = useState(1)
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const { page, handleLoadMore } = useLoadMore(1);
 
+  const { allPages, nftCards } = useFetchNft({
+    setLoading: setIsLoading,
+    page,
+    sort: 'items',
+    on_sale: true,
+  });
+  // const handleLoadMore = useCallback(() => {
+  //   setCurrentPage((prevState) => prevState + 1);
+  // }, []);
   return (
     <div className={cx(styles.ourArtworkGallery, className)}>
       <div className={styles.title}>
@@ -21,42 +31,41 @@ const OurArtworkGallery: FC<Props> = ({ className }) => {
         </H2>
       </div>
       <div className={styles.artCardsWrapper}>
-        {art.map((artPiece: any) => {
+        {nftCards.map((artPiece: any) => {
           const {
-            image,
+            id,
+            media,
             name,
             price,
-            asset,
-            inStockNumber,
-            author,
-            authorAvatar,
-            likesNumber,
+            highest_bid,
+            minimal_bid,
+            currency,
+            creator,
+            like_count,
+            available,
             tags,
           } = artPiece;
           return (
             <ArtCard
+              artId={id}
               key={name}
-              imageMain={image}
+              imageMain={media}
               name={name}
-              price={price}
-              asset={asset}
-              inStockNumber={inStockNumber}
-              author={author}
-              authorAvatar={authorAvatar}
-              likesNumber={likesNumber}
+              price={price || highest_bid || minimal_bid}
+              asset={currency.symbol.toUpperCase()}
+              inStockNumber={available}
+              author={creator.name}
+              authorAvatar={creator.avatar}
+              likesNumber={like_count}
               tags={tags}
             />
           );
         })}
       </div>
       {isLoading && <Loader className={styles.loader} />}
-      {(!isLoading || currentPage >= allPages) && (
+      {(!isLoading || page >= allPages) && (
         <div className={styles.viewMoreBtnWrapper}>
-          <Button
-            color="outline"
-            className={styles.viewMoreBtn}
-            onClick={() => setCurrentPage(currentPage + 1)}
-          >
+          <Button color="outline" className={styles.viewMoreBtn} onClick={handleLoadMore}>
             View More
           </Button>
         </div>
