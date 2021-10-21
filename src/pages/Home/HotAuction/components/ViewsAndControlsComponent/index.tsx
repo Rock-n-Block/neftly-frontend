bffa5ba75ff6a95ffeac66b6743beccaf06a1e2f
TheJuze <dimanikus0106@gmail.com>
2021-10-21 15:19:46 +0300

    fixed imports style

--- src/pages/Home/HotAuction/components/ViewsAndControlsComponent/index.tsx
+++ src/pages/Home/HotAuction/components/ViewsAndControlsComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 import cx from 'classnames';
 import Tooltip from 'rc-tooltip';
 import { observer } from 'mobx-react-lite';
@@ -19,7 +19,7 @@ import reportImg from 'assets/img/icons/report.svg';
 import { useLike } from 'hooks';
 import linkIcon from 'assets/img/icons/link.svg';
 import { numberFormatter } from 'utils';
-import { routes } from '../../../../../appConstants';
+import { routes } from 'appConstants';
 
 type Props = {
   className?: string;
@@ -53,22 +53,22 @@ const ViewsAndControlsComponent: FC<Props> = ({
     !!user.address,
   );
 
-  const [isTooltipVisible, setTooltipVisible] = React.useState(false);
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
 
-  const handleActionEvent = React.useCallback((event: () => void) => {
+  const handleActionEvent = useCallback((event: () => void) => {
     setTooltipVisible(false);
     event();
   }, []);
 
-  const handleBurn = React.useCallback(() => {
+  const handleBurn = useCallback(() => {
     burn.open(nft?.id || 0, nft?.standart || '');
   }, [burn, nft]);
 
-  const handleRemoveFromSale = React.useCallback(() => {
+  const handleRemoveFromSale = useCallback(() => {
     remove.open(nft?.id || 0);
   }, [remove, nft]);
 
-  const handleTransfer = React.useCallback(() => {
+  const handleTransfer = useCallback(() => {
     let available = 0;
     if (Array.isArray(nft?.owners)) {
       available =
@@ -81,11 +81,11 @@ const ViewsAndControlsComponent: FC<Props> = ({
     transfer.open(nft?.id || 0, nft?.standart || '', available);
   }, [transfer, nft, user.id]);
 
-  const handleReport = React.useCallback(() => {
+  const handleReport = useCallback(() => {
     report.open();
   }, [report]);
 
-  const actions = React.useMemo(
+  const actions = useMemo(
     () => [
       {
         name: 'Transfer Token',
@@ -124,7 +124,7 @@ const ViewsAndControlsComponent: FC<Props> = ({
     ],
   );
 
-  const handleCopy = React.useCallback(() => {
+  const handleCopy = useCallback(() => {
     navigator.clipboard.writeText(`${window.location.origin}${routes.nft.link(nft?.id || '')}`);
     toast.info('Copied to Clipboard');
   }, [nft]);
