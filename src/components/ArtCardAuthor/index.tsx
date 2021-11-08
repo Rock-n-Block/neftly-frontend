62b1f497c142e40aa9effb4fa0afa5c15ab8e090
TheJuze <dimanikus0106@gmail.com>
2021-11-08 14:58:08 +0300

    changed ternary expr to expr

--- src/components/ArtCardAuthor/index.tsx
+++ src/components/ArtCardAuthor/index.tsx
@@ -17,11 +17,7 @@ const ArtCardAuthor: FC<IProps> = ({ id, avatar, name, verified = false }) => {
     <div className={styles.artCardAuthor}>
       <Link to={routes.profile.link(id || '')} className={styles.avatar}>
         <img src={avatar} alt="" width={24} height={24} className={styles.avatarImg} />
-        {verified ? (
-          <img src={iconVerifiedMark} alt="verified" className={styles.verified} />
-        ) : (
-          <></>
-        )}
+        {verified && <img src={iconVerifiedMark} alt="verified" className={styles.verified} />}
       </Link>
       <Text className={styles.name} color="inherit">
         {name}
