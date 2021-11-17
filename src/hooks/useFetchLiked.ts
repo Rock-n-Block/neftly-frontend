b940d8162ce6c33b83eea6bf2eda1701f335a998
vKul <valkulchikhin@gmail.com>
2021-11-17 11:51:50 +0300

    fix import order

--- src/hooks/useFetchLiked.ts
+++ src/hooks/useFetchLiked.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { storeApi } from 'services';
 import { INft } from 'typings';
 
