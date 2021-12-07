d2526b86b170e01a4765672a40dcd8c357cc68e1
overkam <superivan0755@gmail.com>
2021-12-07 10:47:38 +0300

    get back back

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { is_production } from '../config';
 
 axios.defaults.baseURL = is_production
-  ? 'https://lessnft.rocknblock.io/api/v1'
-  : 'https://lessnft.rocknblock.io/api/v1';
+  ? 'https://nftcrowd.rocknblock.io/api/v1'
+  : 'https://nftcrowd.rocknblock.io/api/v1';
 
 axios.interceptors.request.use(
   (config) => {
